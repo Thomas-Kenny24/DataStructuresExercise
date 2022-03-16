da73d154689a697e676b9ecc391817e7740afd0d
“Thomas-Kenny24” <dice2310@gmail.com>
2022-03-16 17:02:46 -0400

    completed exercise

---
+++ script.js
@@ -0,0 +1,48 @@
+const jedi = [];
+// 
+jedi[0] = "Luke";
+console.log(jedi);
+// 
+jedi.push('Obi-Wan Kenobi');
+console.log(jedi);
+// 
+jedi.unshift('Yoda');
+console.log(jedi);
+// 
+console.log(jedi[1]);
+// 
+const force = jedi.pop();
+console.log(jedi);
+// 
+const dark = jedi.shift();
+console.log(jedi);
+// 
+const sithLords = [
+    'Darth Vader',
+    'Darth Sidious',
+    'Darth Maul'
+];
+// 
+const imperialOfficers = [
+    'Grand Moff Tarkin',
+    'Orson Krennic'
+];
+// 
+const starWarsVillains = sithLords.concat(imperialOfficers);
+console.log(starWarsVillains);
+// 
+console.log(starWarsVillains.slice(0, 2));
+// 
+const droids = {
+    astromech: 'R2-D2',
+    protocol: 'C-3PO',
+    assassin: 'IG-88'
+};
+console.log(droids);
+// 
+console.log(droids['astromech']);
+// 
+console.log(droids.protocol);
+// 
+droids['assassin'] = 'IG-11';
+console.log(droids);
